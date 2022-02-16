86ad978fe0bba4708574bb1a2a1b10b3428f7be4
bhargav vachhani <bhargavvachhani5@gmail.com>
2022-02-16 21:57:34 +0530

    Country Add Edit Page

--- MultiUserAddressBook/Admin Panel/Country/CountryAddEdit.aspx.cs
+++ MultiUserAddressBook/Admin Panel/Country/CountryAddEdit.aspx.cs
@@ -13,20 +13,38 @@ public partial class MultiUserAddressBook_Admin_Panel_Country_CountryAddEdit : S
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
 
+
+            if (Request.QueryString["CountryID"] != null)
+            {
+                lblMessage.Text = "Edit Mode | CountryID " + Request.QueryString["CountryID"].Trim();
+                FillControls(Convert.ToInt32(Request.QueryString["CountryID"].Trim()));
+            }
+            else
+            {
+                lblMessage.Text = "Add Mode";
+            }
+
+
+
+
+        }
     }
-
+    #region Btn Save
     protected void btnSave_Click(object sender, EventArgs e)
     {
         SqlString strCountryName = SqlString.Null;
         SqlString strCountryCode = SqlString.Null;
-        
+
+
         String strErrorMessage = "";
-       if(txtCountryName.Text.Trim() == "")
+        if (txtCountryName.Text.Trim() == "")
         {
             strErrorMessage += "- Enter Country Name <br />";
         }
-       if(txtCountryCode.Text.Trim() == "")
+        if (txtCountryCode.Text.Trim() == "")
         {
             strErrorMessage += "-Enter Country Code <br />";
         }
@@ -34,41 +52,123 @@ public partial class MultiUserAddressBook_Admin_Panel_Country_CountryAddEdit : S
         {
             strErrorMessage += "-Enter Country Name and Country Code";
         }
-       if(strErrorMessage !="")
+        if (strErrorMessage != "")
         {
-           lblMessage.Text = strErrorMessage;
+            lblMessage.Text = strErrorMessage;
             return;
         }
 
         SqlConnection objConn = new SqlConnection(ConfigurationManager.ConnectionStrings["MultiUserAddressBookConnectionString"].ConnectionString);
-        objConn.Open();
-        SqlCommand objCmd = objConn.CreateCommand();
-        objCmd.CommandType = CommandType.StoredProcedure;
-        objCmd.CommandText = "PR_Counrty_Insert";
+        try
+        {
+            objConn.Open();
+            SqlCommand objCmd = objConn.CreateCommand();
+            objCmd.CommandType = CommandType.StoredProcedure;
+            objCmd.CommandText = "PR_Counrty_Insert";
 
-        if (txtCountryName.Text.Trim() != "")
-        {
-            strCountryName = txtCountryName.Text.Trim();
+            if (txtCountryName.Text.Trim() != "")
+            {
+                strCountryName = txtCountryName.Text.Trim();
+            }
+            if (txtCountryCode.Text.Trim() != "")
+            {
+                strCountryCode = txtCountryCode.Text.Trim();
+            }
+            if (Session["UserID"] != null)
+                objCmd.Parameters.AddWithValue("UserID", Session["UserID"]);
+            objCmd.Parameters.AddWithValue("@CountryName", strCountryName);
+            objCmd.Parameters.AddWithValue("@CountryCode", strCountryCode);
+
+            objCmd.ExecuteNonQuery();
+            objConn.Close();
+            lblMessage.Text = "Data Inserted Succesfully";
+            txtCountryName.Text = "";
+            txtCountryCode.Text = "";
+            txtCountryName.Focus();
+            if (objConn.State == ConnectionState.Open)
+                objConn.Close();
         }
-        if (txtCountryCode.Text.Trim() != "")
+        catch (Exception ex)
         {
-            strCountryCode = txtCountryCode.Text.Trim();
+            lblMessage.Text = ex.Message;
         }
-        if (Session["UserID"] != null)
-            objCmd.Parameters.AddWithValue("UserID", Session["UserID"]);
-        objCmd.Parameters.AddWithValue("@CountryName", strCountryName);
-        objCmd.Parameters.AddWithValue("@CountryCode", strCountryCode);
-        
-        objCmd.ExecuteNonQuery();
-        objConn.Close();
-        lblMessage.Text = "Data Inserted Succesfully";
-        txtCountryName.Text = "";
-        txtCountryCode.Text = "";
-        txtCountryName.Focus();
+        finally
+        {
+            if (objConn.State == ConnectionState.Open)
+                objConn.Close();
+        }
     }
+
+    #endregion Btn Save
+
+    #region Button : cancel
 
     protected void btnCancel_Click(object sender, EventArgs e)
     {
-        Response.Redirect("~/MultiUserAddressBook/Admin Panel/Country/CountryList.aspx",true);
+        Response.Redirect("~/MultiUserAddressBook/Admin Panel/Country/CountryList.aspx", true);
     }
+    #endregion Button : cancel
+
+    #region Fill Controls
+    private void FillControls(SqlInt32 CountryID)
+    {
+        #region Local variable
+        SqlConnection objConn = new SqlConnection(ConfigurationManager.ConnectionStrings["MultiUserAddressBookConnectionString"].ConnectionString.Trim());
+        #endregion Local variable
+        try
+        {
+            #region Connection Open And Command Object
+            if (objConn.State != ConnectionState.Open)
+
+                objConn.Open();
+
+            SqlCommand objCmd = objConn.CreateCommand();
+            objCmd.CommandType = CommandType.StoredProcedure;
+            objCmd.CommandText = "[dbo].[PR_Country_SelectByPK]";
+            objCmd.Parameters.AddWithValue("@CountryID", CountryID.ToString().Trim());
+
+            SqlDataReader objSDR = objCmd.ExecuteReader();
+
+            if (objSDR.HasRows)
+            {
+                while (objSDR.Read())
+                {
+                    if (!objSDR["CountryName"].Equals(DBNull.Value))
+                    {
+                        txtCountryName.Text = objSDR["CountryName"].ToString().Trim();
+                    }
+                    if (!objSDR["CountryCode"].Equals(DBNull.Value))
+                    {
+                        txtCountryCode.Text = objSDR["CountryCode"].ToString().Trim();
+                    }
+                    break;
+                }
+            }
+            else
+            {
+                lblMessage.Text = "No Date Available for the StateID =" + CountryID.ToString();
+            }
+
+            if (objConn.State == ConnectionState.Open)
+                objConn.Close();
+
+
+            #endregion Connection Open And Command Object
+
+        }
+        catch (Exception ex)
+        {
+            lblMessage.Text = ex.Message;
+        }
+        finally
+        {
+            if (objConn.State == ConnectionState.Open)
+            {
+                objConn.Close();
+            }
+        }
+
+    }
+    #endregion Fill Controls
+
 }
